3b356fa18280492d4debbf3cb2b03f82962ea957
dmitryS1666 <dsuschinsky@gmail.com>
2018-05-15 21:30:18 +0100

    Add validation for Request.new form

--- app/assets/javascripts/channels/requests.js
+++ app/assets/javascripts/channels/requests.js
@@ -3,21 +3,45 @@
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 
 var ready = function () {
-  App.cable.subscriptions.create('RequestsChannel', {
-    connected: function () {
-      this.perform('follow');
-    },
+    App.cable.subscriptions.create('RequestsChannel', {
+        connected: function () {
+            this.perform('follow');
+        },
 
-    received: function (data) {
-        var $request_list = $('.requests-list');
-        if(/destroy=true/.test(data)){
-            console.log(data);
-        } else {
-            console.log(data);
-            $request_list.append(data);
+        received: function (data) {
+            var $request_list = $('.requests-list');
+            if(/destroy=true/.test(data)){
+                console.log(data);
+            } else {
+                console.log(data);
+                $request_list.append(data);
+            }
         }
+    })
+};
+
+var validation = function () {
+    if($('#new_request').length) {
+        var input_title = document.getElementById('request_title');
+        var input_body = $('#request_body');
+
+        $('#request_title').bind('input', function() {
+            if(input_title.value.length < 5) {
+                input_title.className += " invalid";
+            } else {
+                input_title.className -= " invalid";
+            }
+        });
+
+        $('#input_body').bind('input', function() {
+            if(input_body.value.length < 10) {
+                input_body.className += " invalid";
+            } else {
+                input_body.className -= " invalid";
+            }
+        });
     }
-  })
 };
 
 $(document).on('turbolinks:load', ready);
+$(document).on('turbolinks:load', validation);
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   has_many :comments
 
-  validates :title, presence: true, length: { minimum: 10 }
-  validates :body, presence: true, length: { minimum: 2 }
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 10 }
   validates :owner_id, presence: true
 end
